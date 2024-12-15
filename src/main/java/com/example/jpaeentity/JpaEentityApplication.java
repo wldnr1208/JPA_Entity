e1f7d05f7ad9a254ec8ea40b4d42de3cd25e06d2
wldnr1208@naver.com <wldnr1208@naver.com>
2024-12-15 18:49:23 +0900

    "JPA_기본_세팅"

---
+++ src/main/java/com/example/jpaeentity/JpaEentityApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaeentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaEentityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaEentityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpaeentity/JpaEentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaeentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaEentityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
